d450cba80d7e0e0f733f4c4cc89a7b0d4f65b508
sijon Biswas <111903975+Sijon20@users.noreply.github.com>
2023-03-12 17:08:30 +0530

    Update data.js

---
+++ pages/api/data.js
@@ -0,0 +1,12 @@
+import * as fs from 'node:fs';
+
+
+export default function handler(req, res) {
+    let data1;
+    fs.readdir('../../data',utf-8, (err, data) => {
+        if (err) throw err;
+        data1 = data
+      });
+    res.status(200).json({name:'hello world'})
+  }
+  
