afc861a3dd338213efd6029b3c1e7134c466c1e3
sijon Biswas <111903975+Sijon20@users.noreply.github.com>
2023-03-12 17:17:40 +0530

    added

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,12 +1,15 @@
-import * as fs from 'node:fs';
+// import * as fs from 'node:fs';
 
 
-export default function handler(req, res) {
-    let data1;
-    fs.readdir('../../data',utf-8, (err, data) => {
-        if (err) throw err;
-        data1 = data
-      });
-    res.status(200).json({name:'hello world'})
-  }
+// export default function handler(req, res) {
+//     // let data1;
+//     // fs.readdir('../../data',utf-8, (err, data) => {
+//     //     if (err) throw err;
+//     //     data1 = data
+//     //   });
+//     res.status(200).json({name:'hello world'})
+//   }
   
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' })
+}
